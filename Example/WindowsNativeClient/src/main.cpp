2da70f28ae0161566ac5129ee5a940fb72b98848
takehara ryo <ryotakehara3586@gmail.com>
2019-10-05 20:02:15 +0900

    - LICENSE を追加 - README.md に現状及び QuicheWrapperExample に関する記載を追加 - QuicheWrapperExample を追加

---
+++ Example/WindowsNativeClient/src/main.cpp
@@ -0,0 +1,27 @@
+﻿#include "QuicheWrapperExample.h"
+
+// サーバの指定
+#if 0
+// 外部サイトを使う場合
+const char* HOST = "quic.aiortc.org";
+//const char* HOST = "blog.cloudflare.com";
+const char* PORT = "443";
+#else
+const char* HOST = "localhost";
+const char* PORT = "4433";
+#endif
+
+int main(int argc, char* argv[])
+{
+	const char* host = HOST;
+	const char* port = PORT;
+	if (2 == argc)
+	{
+		host = argv[1];
+		port = argv[2];
+	}
+
+	QuicheWrapper qw(host,  port);
+	
+	return qw.Execute();
+}
